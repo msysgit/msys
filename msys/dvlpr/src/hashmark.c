fd5b70a135a197dac7779f9305ece079c704dab2
earnie <earnie>
2002-04-19 20:42:22 +0000

    * src: New directory. 	* src/hashmark.c: New file. 	* script/msysdvlpr: Document.

---
+++ msys/dvlpr/src/hashmark.c
@@ -0,0 +1,13 @@
+/*
+ * hashmark.c
+ * Copyright (C) 2002, Earnie Boyd <earnie@users.sf.net>
+ * This file is a part of msysDVLPR
+ */
+
+/* The intention for this file is to create a binary named #.exe and #!.exe */
+
+int
+main (void)
+{
+  return 0;
+}
