522f80da7ec5b75b07d44f79252baee28a71d586
earnie <earnie>
2002-05-13 18:31:46 +0000

    Pristine

---
+++ msys/packages/gzip/1.2.4a/crypt.c
@@ -0,0 +1,6 @@
+/* crypt.c (dummy version) -- do not perform encryption
+ * Hardly worth copyrighting :-)
+ */
+#ifdef RCSID
+static char rcsid[] = "$Id: crypt.c,v 1.1 2002/05/13 18:31:38 earnie Exp $";
+#endif
