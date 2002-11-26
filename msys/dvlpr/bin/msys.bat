62886410f11f54d014be949f7003d74da05cebd1
earnie <earnie>
2002-11-26 01:53:36 +0000

    * bin/msys.bat: Modify to allow automatic expansion of environment 	space for Win98 and WinME. 	Many thanks to Randy Sims, Paul Garceau and Luke Dunstan.

--- msys/dvlpr/bin/msys.bat
+++ msys/dvlpr/bin/msys.bat
@@ -8,13 +8,29 @@ rem File:	    msys.bat
 rem Revision:	    2.0
 rem Revision Date:  April 17th, 2002
 
-rem DON'T CHANGE the value of CYGWIN.
-rem Ok.  Go ahead change it.  Just don't cry on my list if you do.
-set CYGWIN="nowinsymlinks notty notitle binmode nontsec nontea nosmbntsec"
-
 rem ember to set the "Start in:" field of the shortcut.
 rem A value similar to C:\msys\1.0\bin is what the "Start in:" field needs
 rem to represent.
+
+rem ember value of GOTO: is used to know recursion has happened.
+if "%1" == "GOTO:" goto %2
+
+rem ember command.com only uses the first eight characters of the label.
+goto _WindowsNT
+
+rem ember that we only execute here if we are in command.com.
+:_Windows
+
+if "x%COMSPEC%" == "x" set COMSPEC=command.com
+start %COMSPEC% /e:4096 /c %0 GOTO: _Resume %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto EOF
+
+rem ember that we execute here if we recursed.
+:_Resume
+for %%F in (1 2 3) do shift
+
+rem ember that we get here even in command.com.
+:_WindowsNT
 
 if "x%MSYSTEM%" == "x" set MSYSTEM=MINGW32
 if "%1" == "MSYS" set MSYSTEM=MSYS
@@ -48,6 +64,8 @@ exit
 :startsh
 start sh --login -i
 exit
+
+:EOF
 
 rem ChangeLog:
 rem 2002.03.07  Earnie Boyd  mailto:earnie@users.sf.net
@@ -87,4 +105,17 @@ rem	* More Win 9x changes.
 rem
 rem 2002.05.04  Earnie Boyd  mailto:earnie@users.sf.net
 rem	* Remove the SET of USERNAME and HOME.
+rem
+rem 2002.11.18  Earnie Boyd  mailto:earnie@users.sf.net
+rem	* Add command.com detection and restart with a larger environment to
+rem	avoid errors on w9x.
+rem     Many thanks to Randy W. Sims mailto:RandyS@ThePierianSpring.org.
+rem	See Randy's response to "RE: [Mingw-msys] Installation on WindowsME" 
+rem	from 11/06/2002 in the archives of mingw-msys@lists.sf.net.
+rem
+rem 2002.11.19  Paul Garceau  mailto:pgarceau@attbi.com
+rem	* Fix a typo: Change COMPSPEC to COMSPEC.
+rem
+rem 2002.11.25  Earnie Boyd  mailto:earnie@users.sf.net
+rem	* Remove the SET CYGWIN since it doesn't matter any longer.
 rem
