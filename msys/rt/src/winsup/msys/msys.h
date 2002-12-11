cb1701ee5960f7e57a23d9f6ec4f89a2d31d0272
earnie <earnie>
2002-12-11 00:17:54 +0000

    Copy cygwin

---
+++ msys/rt/src/winsup/msys/msys.h
@@ -0,0 +1,4 @@
+void __AbsDllPath( const char *, char *, int );
+extern "C" void AbsDllPath( const char *, char *, int );
+void __AbsExeModPath( char *, int );
+extern "C" void AbsExeModPath( char *, int );
