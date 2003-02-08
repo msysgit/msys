de8dacd43426579d5c0a3c0bc93d13db94ee3098
earnie <earnie>
2003-02-08 14:16:47 +0000

    Merge winsup changes

--- runtime/include/locale.h
+++ runtime/include/locale.h
@@ -19,9 +19,9 @@
  *  DISCLAIMED. This includes but is not limited to warranties of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * $Revision: 1.3 $
- * $Author: dannysmith $
- * $Date: 2001-11-29 04:26:33 $
+ * $Revision: 1.4 $
+ * $Author: earnie $
+ * $Date: 2003-02-08 14:16:47 $
  *
  */
 
@@ -50,6 +50,10 @@
 #define	LC_MAX		LC_TIME
 
 #ifndef RC_INVOKED
+
+/* According to C89 std, NULL is defined in locale.h too.  */
+#define __need_NULL
+#include <stddef.h>
 
 /*
  * The structure returned by 'localeconv'.
