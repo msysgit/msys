c19b42628a60948504959213a01f375f51d9ff03
Earnie Boyd <earnie@users.sourceforge.net>
2003-02-24 21:30:39 +0000

    Merge recent change from ../cygwin

--- winsup/msys/include/cygwin/version.h
+++ winsup/msys/include/cygwin/version.h
@@ -42,10 +42,10 @@ details. */
 	 changes to the DLL and is mainly informative in nature. */
 
   /* The current cygwin version is 1.3.3 */
-  /* The current msys version is 1.0.7 */
+  /* The current msys version is 1.0.9 */
 
 #define CYGWIN_VERSION_DLL_MAJOR 1000
-#define CYGWIN_VERSION_DLL_MINOR 8
+#define CYGWIN_VERSION_DLL_MINOR 9
 
       /* Major numbers before CYGWIN_VERSION_DLL_EPOCH are
 	 incompatible. */
