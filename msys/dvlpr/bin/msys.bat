ee907d652f094b4aaded319c16578e86a58f231a
earnie <earnie>
2004-03-30 11:35:29 +0000

    2004.03.30  Earnie Boyd  mailto:earnie@users.sf.net    	* Add -norxvt or --norxvt switch argument.    	Thanks to Keith Marshal mailto:Keith.Marshall@total.com.    	* Add method to determine absolute path of msys.bat so that we no    	longer need to change to the bin directory.  This allows msys.bat to be    	called from any working directory.    	Thanks to Kevin Mack  mailto:kevin.mack@us.cd-adapco.com

--- msys/dvlpr/bin/msys.bat
+++ msys/dvlpr/bin/msys.bat
@@ -1,12 +1,12 @@
 @echo off
-rem Copyright (C):  2001, 2002  Earnie Boyd
+rem Copyright (C):  2001, 2002, 2003, 2004  Earnie Boyd
 rem   mailto:earnie@users.sf.net
 rem This file is part of Minimal SYStem
 rem   http://www.mingw.org/msys.shtml
 rem
 rem File:	    msys.bat
-rem Revision:	    2.0
-rem Revision Date:  April 17th, 2002
+rem Revision:	    2.2
+rem Revision Date:  March 30th, 2004
 
 rem ember to set the "Start in:" field of the shortcut.
 rem A value similar to C:\msys\1.0\bin is what the "Start in:" field needs
@@ -15,11 +15,15 @@ rem to represent.
 rem ember value of GOTO: is used to know recursion has happened.
 if "%1" == "GOTO:" goto %2
 
+if NOT "x%WD%" == "x" set WD=
+
 rem ember command.com only uses the first eight characters of the label.
 goto _WindowsNT
 
 rem ember that we only execute here if we are in command.com.
 :_Windows
+
+set WD=%0\..\
 
 if "x%COMSPEC%" == "x" set COMSPEC=command.com
 start %COMSPEC% /e:4096 /c %0 GOTO: _Resume %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
@@ -32,15 +36,29 @@ for %%F in (1 2 3) do shift
 rem ember that we get here even in command.com.
 :_WindowsNT
 
+if "x%WD%" == "x" set WD=%~dp0
+
+rem ember Set up option to use rxvt based on value of %1
+if "x%MSYSCON%" == "x" set MSYSCON=rxvt.exe
+if "x%1" == "x-norxvt" set MSYSCON=sh.exe
+if "x%1" == "x--norxvt" set MSYSCON=sh.exe
+if "x%MSYSCON%" == "xsh.exe" shift
+
 if "x%MSYSTEM%" == "x" set MSYSTEM=MINGW32
+if "%1" == "MINGW32" set MSYSTEM=MINGW32
 if "%1" == "MSYS" set MSYSTEM=MSYS
 
 if NOT "x%DISPLAY%" == "x" set DISPLAY=
 
-if EXIST bin\nul cd bin
-if EXIST rxvt.exe goto startrxvt
-if EXIST sh.exe goto startsh
+if "x%MSYSCON%" == "xrxvt.exe" goto startrxvt
+if "x%MSYSCON%" == "xsh.exe" goto startsh
 
+:unknowncon
+echo %MSYSCON% is an unknown option for msys.bat.
+pause
+exit 1
+
+:notfound
 echo Cannot find the rxvt.exe or sh.exe binary -- aborting.
 pause
 exit 1
@@ -48,6 +66,7 @@ exit 1
 rem If you don't want to use rxvt then rename the file rxvt.exe to something
 rem else.  Then sh.exe will be used instead.
 :startrxvt
+if NOT EXIST %WD%bin\rxvt.exe goto notfound
 
 rem Setup the default colors for rxvt.
 if "x%MSYSBGCOLOR%" == "x" set MSYSBGCOLOR=White
@@ -59,11 +78,12 @@ if "%MSYSTEM%" == "MSYS" set FGCOLOR=%MSYSFGCOLOR%
 if "%MSYSTEM%" == "MINGW32" set BGCOLOR=%MINGW32BGCOLOR%
 if "%MSYSTEM%" == "MINGW32" set FGCOLOR=%MINGW32FGCOLOR%
 
-start rxvt -backspacekey  -sl 2500 -fg %FGCOLOR% -bg %BGCOLOR% -sr -fn Courier-12 -tn msys -geometry 80x25 -e /bin/sh --login -i
+start %WD%bin\rxvt -backspacekey  -sl 2500 -fg %FGCOLOR% -bg %BGCOLOR% -sr -fn Courier-12 -tn msys -geometry 80x25 -e /bin/sh --login -i
 exit
 
 :startsh
-start sh --login -i
+if NOT EXIST %WD%bin\sh.exe goto notfound
+start %WD%bin\sh --login -i
 exit
 
 :EOF
@@ -132,3 +152,12 @@ rem 2004.01.30  Earnie Boyd  mailto:earnie@users.sf.net
 rem	* Add -geometry parameter to work around an off by one issue with
 rem       the default values.
 rem	Thanks to Dave Schuyler mailto:parameter@users.sf.net
+rem
+rem 2004.03.30  Earnie Boyd  mailto:earnie@users.sf.net
+rem	* Add -norxvt or --norxvt switch argument.
+rem	Thanks to Keith Marshal mailto:Keith.Marshall@total.com.
+rem	* Add method to determine absolute path of msys.bat so that we no
+rem	longer need to change to the bin directory.  This allows msys.bat to be
+rem	called from any working directory.
+rem	Thanks to Kevin Mack  mailto:kevin.mack@us.cd-adapco.com
+rem
