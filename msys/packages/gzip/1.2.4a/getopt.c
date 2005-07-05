859048a021b9ad772d532dd4315b3d4218d1de0f
earnie <earnie>
2005-07-05 12:20:41 +0000

    * getout.c: Cause a null object when __MSYS__. 	* getout.h: Include unistd.h when __MSYS__.

--- msys/packages/gzip/1.2.4a/getopt.c
+++ msys/packages/gzip/1.2.4a/getopt.c
@@ -20,6 +20,7 @@
    along with this program; if not, write to the Free Software
    Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
+#ifndef __MSYS__
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -753,3 +754,4 @@ main (argc, argv)
 }
 
 #endif /* TEST */
+#endif /* __MSYS__ */
