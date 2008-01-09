54bb4fb01793f943de14dffe9d2774213897ea2a
keithmarshall <keithmarshall>
2008-01-09 22:04:08 +0000

    Retain `.exe' file name extension, if present, in program identity banner displayed by `-help' and `-version' options.

--- catgets/gencat.c
+++ catgets/gencat.c
@@ -1,14 +1,14 @@
 /*
  * gencat.c
  *
- * $Id: gencat.c,v 1.4 2008-01-09 21:55:55 keithmarshall Exp $
+ * $Id: gencat.c,v 1.5 2008-01-09 22:04:08 keithmarshall Exp $
  *
  * Copyright (C) 2006, 2007, 2008, Keith Marshall
  *
  * This file implements the `main' function for the `gencat' program.
  *
  * Written by Keith Marshall  <keithmarshall@users.sourceforge.net>
- * Last modification: 08-Jan-2008
+ * Last modification: 09-Jan-2008
  *
  *
  * This is free software.  It is provided AS IS, in the hope that it may
@@ -149,7 +149,6 @@ int main( int argc, char **argv )
     int ext = strlen( *argv ) - 4;
     if( (ext > 0) && (strcasecmp( *argv + ext, ".exe" ) == 0) )
       *(*argv + ext) = '\0';
-    progname = *argv;
   }
   else
     /* Something went wrong...
@@ -178,6 +177,7 @@ int main( int argc, char **argv )
 	exit( EXIT_SUCCESS );
     }
 
+  progname = *argv;
   cat_index.id = tag;
 
   if( (argc -= optind) > 1 )
@@ -388,4 +388,4 @@ int main( int argc, char **argv )
   return EXIT_SUCCESS;
 }
 
-/* $RCSfile: gencat.c,v $Revision: 1.4 $: end of file */
+/* $RCSfile: gencat.c,v $Revision: 1.5 $: end of file */
