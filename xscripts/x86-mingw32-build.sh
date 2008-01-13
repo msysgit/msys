affcc5b4ba995420c9245968472395d2782c3c25
mgdde <mgdde>
2008-01-13 18:01:44 +0000

    Added support for gdc the GNU C language compiler. Updated versions of w32api, mingw-runtime and binutils.

--- xscripts/x86-mingw32-build.sh
+++ xscripts/x86-mingw32-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # x86-mingw32-build.sh -- vim: filetype=sh
-# $Id: x86-mingw32-build.sh,v 1.1 2006-11-05 23:26:39 keithmarshall Exp $
+# $Id: x86-mingw32-build.sh,v 1.2 2008-01-13 18:01:44 mgdde Exp $
 #
 # Script to guide the user through the build of a GNU/Linux hosted
 # MinGW cross-compiler for Win32.
@@ -98,7 +98,18 @@ $script: stage $STAGE: build $COMPONENT ..."
         for FILE in $GCC_LANGUAGE_OPTIONS
 	do
 	  case $GCC_LANGUAGE_SET in *$FILE*) ;; *) FILE=no ;; esac
-	  case $FILE in 'c++') FILE='g++' ;; f77) FILE=g77 ;; esac
+	  case $FILE in
+	    'c++') FILE='g++' ;;
+	    # d is not (yet) fully integrated with gcc
+	    # to support it anyway requires to apply a special patch
+	    d) FILE=no
+	      pushd gcc-*
+	      test -r ./gcc/d/setup-gcc.sh || pushd gcc && \
+	        $RUN prepare gdc-$GDC_VERSION && popd
+	      ./gcc/d/setup-gcc.sh
+	      popd ;;
+	    f77) FILE=g77 ;;
+	  esac
 	  test $FILE = no || $RUN prepare gcc-$FILE-$GCC_VERSION
 	done
       fi
@@ -159,4 +170,4 @@ cd "$WORKING_DIR/.."; eval $RUN $CLEAN_SLATE_ON_EXIT
 echo "done."
 exit 0
 
-# $RCSfile: x86-mingw32-build.sh,v $Revision: 1.1 $: end of file
+# $RCSfile: x86-mingw32-build.sh,v $Revision: 1.2 $: end of file
