7e5591502c73f919e5335b6e11936904452ef2f8
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-05-11 18:59:58 +0200

    Initial version of the cvs2git script to import MSys
    As cvsimport does not cope well with branches, we need to use something
    better: cvs2git.  Unfortunately, this tool requires a local CVS
    repository, which we do not have.  So let's use cvsclone to get a clone
    and use cvs2git to import the stuff.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd "$(dirname "$0")" &&
+
+CVSROOT=:pserver:anonymous@mingw.cvs.sourceforge.net:/cvsroot/mingw &&
+mkdir -p cvsclone &&
+for i in $(sed -n 's/^\/cvsclone\///p' < .gitignore | grep -v '^out.\*$')
+do
+	(cd cvsclone/ &&
+	 $HOME/cvsclone/cvsclone -d $CVSROOT $i > out.cvsclone.$i 2>&1) ||
+	break
+done &&
+$HOME/cvs2svn/cvs2git --blobfile=msys.blobs --dumpfile=msys.dump \
+	--username=dscho cvsclone/ > out.cvs2git 2>&1 &&
+cat msys.blobs msys.dump | git fast-import > out.fast-import 2>&1 ||
+echo "Failure importing"
