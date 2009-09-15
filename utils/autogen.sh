86c4867a838cb76408b6ea02a3f4f93e89cb568d
cwilso11 <cwilso11>
2009-09-15 01:20:56 +0000

    Restructure for forthcoming 0.4 release.

--- utils/autogen.sh
+++ utils/autogen.sh
@@ -1,10 +1,28 @@
-#! /bin/sh
+#! /bin/bash -e
 
-export WANT_AUTOMAKE_1_6=1
+export WANT_AUTOMAKE=1.11
+usage="Usage: autogen.sh [-c]"
+opt_c=
+while getopts ":c" options; do
+  case $options in
+    c  ) opt_c=-c;;
+    \? ) echo $usage; exit 0;;
+    *  ) echo $usage 1>&2; exit 1;;
+  esac
+done
+shift $(($OPTIND - 1))
 
-aclocal \
-&& autoheader \
-&& automake --gnu --add-missing \
-&& autoconf \
-&& ./configure --host=mingw32 --prefix=/mingw --enable-maintainer-mode
+set -x
+aclocal
+autoheader
+automake --gnu --add-missing
+autoconf
+
+(cd msys-here && ./autogen.sh)
+
+if [ "x${opt_c}" != "x" ]
+then
+  ./configure --host=mingw32 --prefix=/mingw \
+	--enable-maintainer-mode --enable-silent-rules "$@"
+fi
 
