98060edcfd8dcf88c93315c24d2fdd0231f9cee8
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-01-13 22:30:40 +0100

    Use an rsync'ed rather than cvsclone'd mirror of the CVS repository
    cvsclone stopped working, at file strcasecmp.c, with a flex error.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- update.sh
+++ update.sh
@@ -2,16 +2,10 @@
 
 cd "$(dirname "$0")" &&
 
-CVSROOT=:pserver:anonymous@mingw.cvs.sourceforge.net:/cvsroot/mingw &&
-mkdir -p cvsclone &&
-for i in $(sed -n 's/^\/cvsclone\///p' < .gitignore | grep -v '^out.\*$')
-do
-	(cd cvsclone/ &&
-	 $HOME/cvsclone/cvsclone -d $CVSROOT $i > out.cvsclone.$i 2>&1) ||
-	break
-done &&
+RSYNC_SOURCE=mingw.cvs.sourceforge.net::cvsroot/mingw/msys &&
+rsync -av $RSYNC_SOURCE cvs-mirror &&
 $HOME/cvs2svn/cvs2git --blobfile=msys.blobs --dumpfile=msys.dump \
-	--username=dscho cvsclone/ > out.cvs2git 2>&1 &&
+	--username=dscho cvs-mirror/ > out.cvs2git 2>&1 &&
 cat msys.blobs msys.dump | git fast-import > out.fast-import 2>&1 &&
 git push --all origin &&
 git push --tags origin ||
