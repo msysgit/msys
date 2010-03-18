5961cba46c26871949b8c17074194c93d612bfcc
cstrauss <cstrauss>
2010-03-18 01:16:35 +0000

    Fix path translation in presence of components with dots.
    * path.cc (msys_p2w): Search the whole string for a ".."
    component, instead of stopping at the first ".".
    * include/cygwin/version.h: Increment version to 1.0.14.

--- msys/rt/src/winsup/cygwin/include/cygwin/version.h
+++ msys/rt/src/winsup/cygwin/include/cygwin/version.h
@@ -45,9 +45,9 @@ details. */
   /* The current msys version is 1.0.13 */
 
 #define CYGWIN_VERSION_DLL_MAJOR 1000
-#define CYGWIN_VERSION_DLL_MINOR 13
+#define CYGWIN_VERSION_DLL_MINOR 14
 #define DLL_VERSION_MAJOR 1000
-#define DLL_VERSION_MINOR 13
+#define DLL_VERSION_MINOR 14
 
       /* Major numbers before CYGWIN_VERSION_DLL_EPOCH are
 	 incompatible. */
