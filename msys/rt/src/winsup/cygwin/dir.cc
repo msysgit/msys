c6cbcfc8033f4525e03621ad014a74043e647491
cstrauss <cstrauss>
2010-09-16 22:53:14 +0000

    Be more robust when dealing with ambiguous paths and mount points

--- msys/rt/src/winsup/cygwin/dir.cc
+++ msys/rt/src/winsup/cygwin/dir.cc
@@ -91,7 +91,7 @@ opendir (const char *dirname)
       goto failed;
     }
 
-  if (stat (real_dirname, &statbuf) == -1)
+  if (stat (dirname, &statbuf) == -1)
     goto failed;
 
   if (!(statbuf.st_mode & S_IFDIR))
