3807473204556fb1ca524dd09b32c76ac07825c9
cstrauss <cstrauss>
2010-09-28 22:50:32 +0000

    Increment version to 1.0.16.

--- msys/rt/src/winsup/cygwin/include/cygwin/version.h
+++ msys/rt/src/winsup/cygwin/include/cygwin/version.h
@@ -42,12 +42,12 @@ details. */
 	 changes to the DLL and is mainly informative in nature. */
 
   /* The current cygwin version is 1.3.3 */
-  /* The current msys version is 1.0.15 */
+  /* The current msys version is 1.0.16 */
 
 #define CYGWIN_VERSION_DLL_MAJOR 1000
-#define CYGWIN_VERSION_DLL_MINOR 15
+#define CYGWIN_VERSION_DLL_MINOR 16
 #define DLL_VERSION_MAJOR 1000
-#define DLL_VERSION_MINOR 15
+#define DLL_VERSION_MINOR 16
 
       /* Major numbers before CYGWIN_VERSION_DLL_EPOCH are
 	 incompatible. */
